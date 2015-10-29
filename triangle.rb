caff33a3e087df2e4c5a45dcd2c9a451cea39bfe
Colter Giem <retrieverofdestiny@gmail.com>
2015-10-28 20:30:36 -0600

    Finished triangles

--- triangle.rb
+++ triangle.rb
@@ -2,25 +2,28 @@ require_relative "shape"
 
 class Rectangle
 
-	attr_reader :x, :y, :width, :length, :color, :zorder
+    attr_reader :x, :y, :side_length, :color, :zorder
 
-	def initialize(x,y,width,length)
+    def initialize(x,y,side_length)
 		@zorder = 1
 		@color = Gosu::Color.argb(0xff_ff0000)
 		@x = x
 		@y = y
-		@width = width
-		@length = length
+        	@side_length = side_length
+        	@height = (Math.sqrt(3) * side_length) / 2
 	end
 
 	def draw_shape
-		Gosu.draw_line(x, y, color, x + width, y, color, zorder)
-		Gosu.draw_line(x + width, y, color, x + (width / 2), y + height, color, zorder)
-		Gosu.draw_line(x + (width / 2), y + height, color, x, y, color, zorder)
+        Gosu.draw_line(x, y, color, x + side_length, y, color, zorder)
+        Gosu.draw_line(x + side_length, y, color, x / 2, y + height, color, zorder)
+	Gosu.draw_line(x / 2, y + height, color, x, y, color, zorder)
 
-		for i in 0...width
-			Gosu.draw_line(x + i, y + length, color, x + i, y, color, zorder)
+        for i in 0...side_length / 2
+	    Gosu.draw_line(x + i, y, color, x + i, y + (Math.sqrt(3) * i), color, zorder)
 		end
+        for i in side_length / 2...side_length
+            Gosu.draw_line(x + i, y, color, x + i, y + height - (Math.sqrt(3) * i), color, zorder)
+        end
 		
 	end
 end
