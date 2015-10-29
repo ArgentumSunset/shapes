0ead2794719e1849894fa237cd57e66829440549
Colter Giem <retrieverofdestiny@gmail.com>
2015-10-28 20:32:56 -0600

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require "gosu"
 require_relative "shape"
 require_relative "rectangle"
+require_relative "triangle"
+require_relative "square"
 
 class GameWindow < Gosu::Window
 
@@ -14,7 +16,9 @@ class GameWindow < Gosu::Window
 
 	def draw
 		rec = Rectangle.new(90,180,10,300)
+        	tri = Triangle.new(300,300,50)
 		rec.draw_shape
+        	tri.draw_shape
 	end
 
 end
